060a3f5057a2857e035bd7701ddf8a9047b54bc1
pulindu <pulindujanith@gmail.com>
2025-01-21 00:57:11 +0530

    remove exclaimation mark

--- src/utils/samples.tsx
+++ src/utils/samples.tsx
@@ -1,3 +1,3 @@
 export const description =
-  "Welcome! to my photography space! I'm passionate about capturing moments that tell a story and bring out the beauty in everyday life. Photography has always been a hobby of mine, and I love sharing the photos I take during my free time. On this site, you'll find a collection of my work, and I'd love for you to explore, comment, and engage with the images that resonate with you. Thanks for visiting, and I hope my photos inspire you as much as they've inspired me.";
+  "Welcome to my photography space! I'm passionate about capturing moments that tell a story and bring out the beauty in everyday life. Photography has always been a hobby of mine, and I love sharing the photos I take during my free time. On this site, you'll find a collection of my work, and I'd love for you to explore, comment, and engage with the images that resonate with you. Thanks for visiting, and I hope my photos inspire you as much as they've inspired me.";
 
